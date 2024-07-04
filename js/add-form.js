0000c008ccdc2b10b83b350163e44163518c7be7
joselmuziotti <joselmuziotti@gmail.com>
2024-07-04 16:45:42 -0500

    Se realizan cambios y css para card

--- js/add-form.js
+++
@@ -1,6 +0,0 @@
-function nombreDelArchivo() {
-    const fileInput = document.querySelector('.input-file input[type="file"]');
-    const fileNameInput = document.getElementById('imagen');
-    const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : '';
-    fileNameInput.value = fileName;
-}
