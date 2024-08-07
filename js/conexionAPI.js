905632702966a5aa5ee9bb9ef035a7895b0bec23
joselmuziotti <joselmuziotti@gmail.com>
2024-08-07 15:33:15 -0500

    Add fake API REST

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,12 +1,12 @@
 async function listarProductos(){
-    const conexion = await fetch("http://localhost:3001/productos");
+    const conexion = await fetch("https://my-json-server.typicode.com/joselmuziotti/TecGeek-AluraGeek-/productos");
     const conexionConvertida = conexion.json();
     
     return conexionConvertida;
 }
 
 async function agregarProducto(imagen, nombre, valor){
-    const conexion = await fetch("http://localhost:3001/productos", {
+    const conexion = await fetch("https://my-json-server.typicode.com/joselmuziotti/TecGeek-AluraGeek-/productos", {
         method: 'POST',
         headers: {"Content-type":"application/json"},
         body: JSON.stringify({
@@ -25,14 +25,14 @@ async function agregarProducto(imagen, nombre, valor){
 }
 
 async function buscarProducto(palabraClave){
-    const conexion = await fetch(`http://localhost:3001/productos?q=${palabraClave}`);
+    const conexion = await fetch(`https://my-json-server.typicode.com/joselmuziotti/TecGeek-AluraGeek-/productos?q=${palabraClave}`);
     const conexionConvertida = conexion.json();
 
     return conexionConvertida;
 }
 
 async function borrarCard(cardId){
-    const conexion = await fetch(`http://localhost:3001/productos/${cardId}`, {
+    const conexion = await fetch(`https://my-json-server.typicode.com/joselmuziotti/TecGeek-AluraGeek-/productos/${cardId}`, {
         method: 'DELETE',
     });
 
